b164b51d86defa1d5991f2fd2b62b21c2cf5b086
Martin Tajur <martin@tajur.ee>
2016-04-05 18:07:10 +0300

    version 2.0.0 - support for multiple service structures, capable of storing state in browser localStorage, bootstrap driven UI

---
+++ public/js/service-navigation.js
@@ -0,0 +1,169 @@
+$(function() {
+	var services = {},
+		storedServices,
+		currentService = null;
+
+	$('.service-dd-link').each(function(i, srv) {
+		var $srv = $(srv),
+			id = parseInt($srv.data('service-id'), 10);
+		if (!$srv.data('service-id')) return;
+
+		services[id] = {
+			id: id,
+			ddItem: $srv,
+			name: $srv.text(),
+			remove: removeService.bind(services[id]),
+			jsonContainer: $('.json-input[data-service-id="' + id + '"]'),
+			focused: false
+		};
+	});
+
+	if (window.localStorage['rsv-services']) {
+		// user has previously stored service structures, render these
+		try {
+			storedServices = JSON.parse(window.localStorage['rsv-services']);
+		} catch (e) {
+			window.localStorage['rsv-services'] = JSON.stringify({});
+		}
+		_.each(storedServices, function(srv) {
+			addService(srv);
+		});
+	} else {
+		// user has no previously stored service structures, render a default example instead
+		d3.json('/example.json', function(err, json) {
+			if (err) throw err;
+
+			addService({
+				id: 1,
+				name: 'example-service-1',
+				json: JSON.stringify(json, null, 3)
+			});
+		});
+	}
+
+	var focusedService = _.find(_.values(services), { 'focused': true });
+
+	if (!focusedService) {
+		focusedService = Object.keys(services).shift();
+	} else {
+		focusedService = focusedService.id;
+	}
+
+	switchService(focusedService);
+	handleDeleteButtonState();
+
+	$('#add-service-link').on('click', addService);
+	$('#button-delete').on('click', function() {
+		if (Object.keys(services).length <= 1) return;
+		if (window.confirm('Are you sure you wish to delete ' + currentService.name + ' service?')) {
+			currentService.remove();
+		}
+	});
+	$('#button-rename').on('click', handleRename);
+	$('#current-service-title').on('dblclick', handleRename);
+
+	$(document).on('click', '.service-dd-link a', function(e) {
+		switchService(parseInt($(e.target).parent().data('service-id'), 10));
+	});
+
+	$(document).on('rsv-service-json-updated rsv-service-added rsv-service-removed rsv-service-switched rsv-service-renamed', handleDeleteButtonState);
+
+	$(document).on('rsv-service-renamed', function() {
+		currentService.ddItem.find('a').text(currentService.name);
+		$('#current-service-title').text(currentService.name);
+	});
+
+	$(document).on('rsv-service-json-updated rsv-service-added rsv-service-removed rsv-service-switched rsv-service-renamed', function() {
+		if (!window.localStorage) return;
+		var srvObj = {};
+
+		_.each(services, function(srv) {
+			srvObj[srv.id] = {
+				id: srv.id,
+				name: srv.name,
+				json: srv.jsonContainer.val(),
+				focused: srv.focused
+			};
+		});
+
+		window.localStorage['rsv-services'] = JSON.stringify(srvObj);
+	});
+
+	function handleDeleteButtonState() {
+		if (Object.keys(services).length <= 1) {
+			$('#button-delete').prop('disabled', true);
+		} else {
+			$('#button-delete').prop('disabled', false);
+		}
+	}
+
+	function handleRename() {
+		var newname = null;
+		if ((newname = window.prompt('Enter new name for ' + currentService.name, currentService.name)) !== null) {
+			renameService(currentService.id, newname);
+		}
+	}
+
+	function switchService(id) {
+		if (!services[id]) return;
+		_.each(services, function(srv, key) {
+			services[key].focused = false;
+		});
+		services[id].focused = true;
+		currentService = services[id];
+		$('.service-dd-link').removeClass('active');
+		$('.service-dd-link[data-service-id="' + id + '"]').addClass('active');
+		$('#current-service-title').text(currentService.name);
+		$('.json-input').hide();
+		$('.json-input[data-service-id="' + id + '"]').show();
+		$(document).trigger('rsv-service-switched', currentService);
+	}
+
+	function getLastId() {
+		return Object.keys(services).sort().pop();
+	}
+
+	function addService(data) {
+		if (!data) data = {};
+
+		var lastId = getLastId(),
+			id = data.id || parseInt(lastId, 10) + 1,
+			srv,
+			defaultName = 'example-service-' + id;
+
+		if (data.id && services[data.id]) {
+			if (data.name) renameService(data.id, data.name || defaultName);
+			if (data.json) services[data.id].jsonContainer.val(data.json);
+			srv = services[data.id];
+		} else {
+			srv = {
+				id: data.id || id,
+				ddItem: $(services[lastId].ddItem).clone().attr('id', 'service-selector-' + id).attr('data-service-id', id).insertBefore('#service-selectors-separator'),
+				name: data.name || defaultName,
+				jsonContainer: $(services[lastId].jsonContainer).clone().attr('id', 'service-selector-' + id).attr('data-service-id', id).insertAfter('.json-input:last').val(data.json || '{\n   "exchanges": [],\n   "queues": []\n}')
+			};
+		}
+
+		srv.ddItem.find('a').html(srv.name);
+		services[id] = srv;
+		services[id].remove = removeService.bind(srv);
+		if (!data.id || data.focused) switchService(id);
+
+		$(document).trigger('rsv-service-added', srv);
+		services[id].jsonContainer.change();
+	}
+
+	function renameService(id, newname) {
+		services[id].name = newname;
+		$(document).trigger('rsv-service-renamed', services[id]);
+	}
+
+	function removeService() {
+		if (Object.keys(services).length <= 1) return;
+		this.ddItem.remove();
+		this.jsonContainer.remove();
+		$(document).trigger('rsv-service-removed', this.id);
+		delete services[this.id];
+		switchService(getLastId());
+	}
+});
--- public/js/visualizer.js
+++ public/js/visualizer.js
@@ -1,142 +1,173 @@
-var input = document.getElementById('input'),
-	delayTimer = null;
+$(function() {
+	var delayTimer = null;
 
-input.addEventListener('change', reactToChange);
-input.addEventListener('keyup', reactToChange);
+	$(document).on('rsv-service-added rsv-service-removed rsv-service-switched', bindTextareas);
 
-d3.json('/example.json', renderDefault);
-
-function renderDefault(err, str) {
-	input.value = JSON.stringify(str, null, 3);
-	renderStructure(str);
-}
-
-function reactToChange() {
-	var data;
-	try {
-		data = JSON.parse(this.value);
+	function bindTextareas() {
+		$('.json-input').off('change keyup').on('change keyup', reactToChange);
+		$('.json-input').off('blur').on('blur', function() {
+			try {
+				$(this).val(JSON.stringify(JSON.parse($(this).val()), null, 3)).change();
+			} catch (e) {
+				// ignore errors
+			}
+			reactToChange();
+		});
 	}
-	catch (err) {
-		console.warn(err);
-	}
-	renderStructureWithDelay(data);
-}
 
-function renderStructureWithDelay(structure) {
-	if (delayTimer) {
-		clearTimeout(delayTimer);
-	}
-	delayTimer = setTimeout(function() {
-		renderStructure(structure);
-	}, 200);
-}
+	function reactToChange() {
+		var combinedData = {
+			exchanges: [],
+			queues: []
+		};
 
-function renderStructure(structure) {
-	var chart = d3
-			.select("#chart")
-			.html('')
-			.append('svg')
-			.chart('Sankey.Path'),
-		exchangeFmt = _.template('Exchange: <%- name %>'),
-		queueFmt = _.template('Queue: <%- name %>'),
-		bindingFmt = _.template('→ <%- routing %> →'),
-		deadLetterBindingFmt = _.template('← x-dead-letter-exchange: <%- data["x-dead-letter-exchange"] %> (<%- data["x-message-ttl"] %> ms) ←', { variable: 'data' });
+		$('.json-input').each(function() {
+			var data = {};
 
-	chart
-		.nodeWidth(20)
-		.nodePadding(25)
-		.iterations(10)
-		.spread(true)
-		.name(function(n) { return n.name; })
-		.colorNodes(function(name, node) {
-			if (name && name.toLowerCase().indexOf('queue') > -1) return '#f6b26b';
-			if (name && name.toLowerCase().indexOf('→') > -1) return '#9ec4e8';
-			if (name && name.toLowerCase().indexOf('x-dead-letter') > -1) return '#f00';
-			return '#fff176';
-		})
-		.colorLinks(function(link) {
-			return '#999';
+			try {
+				data = JSON.parse($(this).val());
+			}
+			catch (err) {
+				$(this).addClass('has-error');
+				console.warn(err);
+			}
+			$(this).removeClass('has-error');
+
+			if (data && data.exchanges) {
+				_.each(data.exchanges, function(exchange, i) {
+					if (_.find(combinedData.exchanges, { name: exchange.name })) {
+						data.exchanges.splice(i);
+						// TODO: validate exchange properties and alert if there is a mismatch
+					}
+				});
+				combinedData.exchanges = combinedData.exchanges.concat(data.exchanges);
+			}
+			if (data && data.queues) {
+				_.each(data.queues, function(queue, i) {
+					var existingQueue = _.find(combinedData.queues, { name: queue.name });
+					if (existingQueue) {
+						existingQueue.bindings = existingQueue.bindings.concat(queue.bindings);
+						data.queues.splice(i);
+						// TODO: validate queue properties and alert if there is a mismatch
+					}
+				});
+				combinedData.queues = combinedData.queues.concat(data.queues);
+			}
+			$(document).trigger('rsv-service-json-updated', { id: $(this).data('service-id'), json: data });
 		});
 
-	var nodes = [],
-		bindings = [],
-		links = [];
+		renderStructureWithDelay(combinedData);
+	}
 
-	structure.exchanges.forEach(function(exchange, key) {
-		exchange.name = exchangeFmt(exchange);
-		exchange._index = nodes.push(exchange) - 1;
-	});
-
-	structure.queues.forEach(function(queue) {
-		queue.name = queueFmt(queue);
-		queue._index = nodes.push(queue) - 1;
-		if (queue.bindings instanceof Array) {
-			queue.bindings.forEach(function(binding) {
-				binding._target = queue;
-				binding.name = bindingFmt(binding);
-				binding._source = _.find(nodes, { name: exchangeFmt({name: binding.exchange}) });
-				bindings.push(binding);
-				binding._index = nodes.push(binding) - 1;
-			});
+	function renderStructureWithDelay(structure) {
+		if (delayTimer) {
+			clearTimeout(delayTimer);
 		}
-		if (queue.options && queue.options.arguments && queue.options.arguments['x-dead-letter-exchange']) {
-			var deadLetterBinding = {
-				_source: _.find(nodes, { name: exchangeFmt({name: queue.options.arguments['x-dead-letter-exchange'] }) }),
-				name: deadLetterBindingFmt(queue.options.arguments),
-				_target: queue,
-				_value: 0.5
-			};
-			bindings.push(deadLetterBinding);
-			deadLetterBinding._index = nodes.push(deadLetterBinding) - 1;
-		}
-	});
+		delayTimer = setTimeout(function() {
+			renderStructure(structure);
+		}, 200);
+	}
 
-	bindings.forEach(function(binding) {
-		// look up source...
-		var linkSource = {},
-			linkTarget = {};
-
-		if (!binding._source) {
-			console.warn('Source not found for binding ' + binding.name);
+	function renderStructure(structure) {
+		if (!structure || !structure.exchanges || !structure.queues) {
 			return;
 		}
 
-		linkSource.source = binding._source._index;
-		linkSource.target = binding._index;
-		linkSource.value = binding._value || 1;
+		$('#chart').css('height', Math.max(250, Math.max(structure.exchanges.length, structure.queues.length) * 60));
 
-		// ...and then target
-		var targetCandidate = _.find(nodes, { name: binding._target.name });
+		var chart = d3
+				.select('#chart')
+				.html('')
+				.append('svg')
+				.chart('Sankey.Path'),
+			exchangeFmt = _.template('Exchange: <%- name %>'),
+			queueFmt = _.template('Queue: <%- name %>'),
+			bindingFmt = _.template('→ <%- routing %> →'),
+			deadLetterBindingFmt = _.template('← x-dead-letter-exchange: <%- data["x-dead-letter-exchange"] %> (<%- data["x-message-ttl"] %> ms) ←', { variable: 'data' });
 
-		if (!targetCandidate || !targetCandidate._index) {
-			throw new Error('Target not found for binding ' + binding.name);
-		}
+		chart
+			.nodeWidth(20)
+			.nodePadding(25)
+			.iterations(60)
+			.spread(true)
+			.name(function(n) { return n.name; })
+			.colorNodes(function(name, node) {
+				if (name && name.toLowerCase().indexOf('queue') > -1) return '#f6b26b';
+				if (name && name.toLowerCase().indexOf('→') > -1) return '#9ec4e8';
+				if (name && name.toLowerCase().indexOf('x-dead-letter') > -1) return '#f00';
+				return '#fff176';
+			})
+			.colorLinks(function(link) {
+				return '#999';
+			});
 
-		linkTarget.source = binding._index;
-		linkTarget.target = targetCandidate._index;
-		linkTarget.value = binding._value || 1;
+		var nodes = [],
+			bindings = [],
+			links = [];
 
-		console.log('Creating a link: ' + binding._source.name + ' --> ' + binding.name + ' --> ' + targetCandidate.name);
+		structure.exchanges.forEach(function(exchange, key) {
+			exchange.name = exchangeFmt(exchange);
+			exchange._index = nodes.push(exchange) - 1;
+		});
 
-		links.push(linkSource);
-		links.push(linkTarget);
-	});
+		structure.queues.forEach(function(queue) {
+			queue.name = queueFmt(queue);
+			queue._index = nodes.push(queue) - 1;
+			if (queue.bindings instanceof Array) {
+				queue.bindings.forEach(function(binding) {
+					binding._target = queue;
+					binding.name = bindingFmt(binding);
+					binding._source = _.find(nodes, { name: exchangeFmt({name: binding.exchange}) });
+					bindings.push(binding);
+					binding._index = nodes.push(binding) - 1;
+				});
+			}
+			if (queue.options && queue.options.arguments && queue.options.arguments['x-dead-letter-exchange']) {
+				var deadLetterBinding = {
+					_source: _.find(nodes, { name: exchangeFmt({name: queue.options.arguments['x-dead-letter-exchange'] }) }),
+					name: deadLetterBindingFmt(queue.options.arguments),
+					_target: queue,
+					_value: 0.5
+				};
+				bindings.push(deadLetterBinding);
+				deadLetterBinding._index = nodes.push(deadLetterBinding) - 1;
+			}
+		});
 
-	var chartData = {
-		nodes: nodes,
-		links: links
-	};
+		bindings.forEach(function(binding) {
+			// look up source...
+			var linkSource = {},
+				linkTarget = {};
 
-	console.log(chartData);
+			if (!binding._source) {
+				console.warn('Source not found for binding ' + binding.name);
+				return;
+			}
 
-	nodes.forEach(function(n) {
-		console.log(n._index + ': ' + n.name);
-	});
+			linkSource.source = binding._source._index;
+			linkSource.target = binding._index;
+			linkSource.value = binding._value || 1;
 
-	links.forEach(function(n) {
-		console.log(n.source + '-->' + n.target);
-	});
+			// ...and then target
+			var targetCandidate = _.find(nodes, { name: binding._target.name });
 
-	chart.draw(chartData);
+			if (!targetCandidate || !targetCandidate._index) {
+				throw new Error('Target not found for binding ' + binding.name);
+			}
 
-}
+			linkTarget.source = binding._index;
+			linkTarget.target = targetCandidate._index;
+			linkTarget.value = binding._value || 1;
+
+			links.push(linkSource);
+			links.push(linkTarget);
+		});
+
+		var chartData = {
+			nodes: nodes,
+			links: links
+		};
+
+		chart.draw(chartData);
+	}
+});
